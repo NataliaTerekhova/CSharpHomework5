572cddd19d1e023ee37bbc63096a86c7fe00e84e
Natalia <natalia_terekhova@yahoo.com>
2023-01-31 11:23:36 +0300

    Added Task 3

---
+++ Homework5/Task3/Program.cs
@@ -0,0 +1,56 @@
+﻿/*Задайте массив вещественных чисел. 
+Найдите разницу между максимальным и минимальным элементов массива.*/
+
+int [] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine(GetMax(arr) + " - это максимальное число");
+System.Console.WriteLine(GetMin(arr) + " - это минимальное число");
+System.Console.WriteLine(GetMax(arr) - GetMin(arr) + " - это разница между максимальным и минимальным числом");
+
+int GetMax(int[]array)
+{
+    int max=array[0];
+    for (int i=1; i<array.Length; i++) 
+    {
+    
+        if (array[i]>max)
+        {
+            max=array[i];
+        }
+    }
+    return max;
+}
+
+int GetMin(int[]array)
+{
+    int min=array[0];
+    for (int i=1; i<array.Length; i++) 
+    {
+    
+        if (array[i]<min)
+        {
+            min=array[i];
+        }
+    }
+    return min;
+}
+
+
+int[]GetRandomArray(int length)
+{
+    int[]result = new int[length];
+    for (int i=0; i<length; i++)
+    {
+        result[i]= new Random().Next(1,11);
+    }
+    return result;
+}
+
+void PrintArray (int[]arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write(item + ", ");
+    }
+    System.Console.WriteLine();
+}
