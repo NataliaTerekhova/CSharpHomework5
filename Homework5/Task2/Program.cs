c4d7a3f97a926e984348f27d47102f6532c56002
Natalia <natalia_terekhova@yahoo.com>
2023-01-31 10:59:26 +0300

    Added Task 2

---
+++ Homework5/Task2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*Задайте одномерный массив, заполненный случайными числами. 
+Найдите сумму элементов, стоящих на нечётных позициях. */
+
+int [] arr = GetRandomArray(10);
+PrintArray(arr);
+System.Console.WriteLine(GetSumOfOddIndex(arr));
+
+int GetSumOfOddIndex (int[]array)
+{
+    int sum = 0; 
+    for (int i=0; i<array.Length; i++) 
+    { 
+        if (i%2==1)
+        {
+         sum = sum + array[i];
+        }
+    }
+return sum;
+} 
+
+int[]GetRandomArray(int length)
+{
+    int[]result = new int[length];
+    for (int i=0; i<length; i++)
+    {
+        result[i]= new Random().Next(1,11);
+    }
+    return result;
+}
+
+void PrintArray (int[]arr)
+{
+    foreach (int item in arr)
+    {
+        System.Console.Write(item + ", ");
+    }
+    System.Console.WriteLine();
+}
